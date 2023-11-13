d87cb80b5867297a60052a0813e1bf689e289bcb
Sumukh Prasad <sumukhprasad@icloud.com>
2023-11-13 20:07:08 +0530

    Initial Commit

---
+++ Code Playgrounds/Code_PlaygroundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_PlaygroundsApp.swift
+//  Code Playgrounds
+//
+//  Created by Sumukh Prasad on 13/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Code_PlaygroundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
