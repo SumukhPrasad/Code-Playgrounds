33356aee691da404c7e131286a1c2e69f3a4f0b0
Sumukh Prasad <sumukhprasad@icloud.com>
2023-11-14 08:40:04 +0530

    Added base UI.

--- Code Playgrounds/ContentView.swift
+++ Code Playgrounds/ContentView.swift
@@ -8,17 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var windowTitleString: String = "Code Playgrounds"
+    @State private var lang = 1
+    @State private var code = ""
+    @State var showingPanel = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextEditor(text: $code)
+                .foregroundStyle(.secondary)
+                .monospaced()
+            
+            HStack(
+                    alignment: .top,
+                    spacing: 10
+                ) {
+                    Button("panel") {
+                        showingPanel.toggle()
+                    }
+                    .floatingPanel(isPresented: $showingPanel, content: {
+                        ZStack {
+                            VisualEffectView(material: .sidebar, blendingMode: .behindWindow)
+                        }
+                    })
+                }
+            
+            TextEditor(text: $code)
+                .foregroundStyle(.secondary)
+                .monospaced()
         }
-        .padding()
+        
+        .toolbar {
+            Text(windowTitleString)
+            
+            Picker(selection: $lang, label: Text("Language"))
+            {
+                Text("Python").tag(1)
+                Text("Swift").tag(2)
+            }
+            
+            Button(action: {})
+            {
+                Label("Run", systemImage: "hammer")
+            }
+            
+            Button(action: {})
+            {
+                Label("Open REPL", systemImage: "apple.terminal")
+            }
+        }
+        
     }
 }
 
+
+/*
 #Preview {
     ContentView()
 }
+*/
